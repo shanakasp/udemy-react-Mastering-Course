e0954fd0f9ff74a1c370d6b572c141ddad8fba94
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-05-28 10:33:10 +0530

    modified Home.js in src

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,17 @@
+import { useState } from "react";
+
 const Home = () => {
+  const [name, setname] = useState("Shanaka");
+
+  const handleClick = () => {
+    setname("Prince");
+  };
   return (
     <div className="Home">
       <h2>Home Page</h2>
+
+      <p>{name}</p>
+      <button onClick={handleClick}>HEllo Plz Click</button>
     </div>
   );
 };
