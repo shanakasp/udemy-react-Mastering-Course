e1d7f2cd738867a1c0847cf7998d486e28117e1b
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-05-28 10:20:46 +0530

    modified App.js and index.css in src

--- src/App.js
+++ src/App.js
@@ -9,10 +9,52 @@ function App() {
       <Navbar></Navbar>
       <div className="content">App Component</div>
       <Home></Home>
+      <div className="example">
+        {" "}
+        <Example Name="User ABC" Age="22" Job="Carpenter"></Example>
+        <Example Name="User ABC" Age="22" Job="Carpenter"></Example>
+      </div>
+
       <h2>{title}</h2>
       <p>{Math.random() * 10}</p>
     </div>
   );
 }
+
+const Example = (props) => {
+  return (
+    <div>
+      <h3>Name: {props.Name}</h3>
+      <h3>Age: {props.Age}</h3>
+      <h3>Job: {props.Job}</h3>
+    </div>
+  );
+};
+
+// const Example = () => {
+//   return (
+//     <div>
+//       <div>
+//         <h3>Name : User ABC</h3>
+//         <h3>Age: 22</h3>
+//         <h3>Job: Carpenter</h3> <hr />
+//       </div>
+
+//       <div>
+//         <h3>Name : User ABC</h3>
+//         <h3>Age: 22</h3>
+//         <h3>Job: Carpenter</h3>
+//         <hr />
+//       </div>
+
+//       <div>
+//         <h3>Name : User ABC</h3>
+//         <h3>Age: 22</h3>
+//         <h3>Job: Carpenter</h3>
+//         <hr />
+//       </div>
+//     </div>
+//   );
+// };
 
 export default App;
